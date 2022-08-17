fe68f5f5c6c428bd8285881e2c9d96c80ddaf0ce
troyb.eth <troy.burmeister@gmail.com>
2022-08-17 13:28:50 -0600

    improve script (#11)

--- src/scripts/get_blocks_mainnet.js
+++ src/scripts/get_blocks_mainnet.js
@@ -1,10 +1,15 @@
 import EthDater from 'ethereum-block-by-date';
 import { ethers } from 'ethers';
+import axios from 'axios';
 import 'dotenv/config';
 import fs from 'fs';
 
 // set some constants
 const NETWORK = 'optimism-mainnet'; // mainnet
+const DIR_NAME = 'data';
+const FILE_NAME = 'blocks_mainnet.json';
+const PERIOD = 3;
+const PERIOD_SECONDS = PERIOD * 60 * 60 * 1000;
 
 // set up the objects
 const provider = new ethers.providers.JsonRpcProvider(
@@ -13,21 +18,36 @@ const provider = new ethers.providers.JsonRpcProvider(
 
 const dater = new EthDater(provider);
 
-// get the blocks and log to console
+// get the blocks and write to local directory
 (async () => {
-    console.log('Getting blocks...');
-    const today = new Date();
+    // pull the existing file from fleek
+    const existingBlocksRequest = await axios.get(
+        `${process.env.FLEEK_BASE_URL}/${DIR_NAME}/${FILE_NAME}`
+    );
+    const existingBlocks = existingBlocksRequest?.data ?? [];
+    // set date boundaries
+    let firstDate;
+    if (existingBlocksRequest.status === 200 && existingBlocksRequest.data.length > 0) {
+        firstDate = new Date(existingBlocks[existingBlocks.length - 1].date);
+    } else {
+        firstDate = new Date('2022-08-01T00:00:00Z');
+    }
+    const startTime = new Date(firstDate.setTime(firstDate.getTime() + PERIOD_SECONDS)).toISOString();
+    const endTime = new Date().toISOString();
 
-    let blocks = await dater.getEvery(
+    console.log(`Getting blocks from ${startTime} to ${endTime}`);
+
+
+    let newBlocks = await dater.getEvery(
         'hours',
-        '2022-08-01T00:00:00Z',
-        today.toISOString(),
-        3,
+        startTime,
+        endTime,
+        PERIOD,
         true,
         false
     );
 
-    blocks = blocks.map(({ date, timestamp, block }) => {
+    newBlocks = newBlocks.map(({ date, timestamp, block }) => {
         const ts = new Date(date);
         return {
             date,
@@ -36,17 +56,21 @@ const dater = new EthDater(provider);
             ts: ts.getTime()
         }
     })
+
+    const blocks = [
+        ...existingBlocks,
+        ...newBlocks
+    ];
     console.log(`Blocks received: ${blocks.length}`);
+
+    if (!fs.existsSync(`./${DIR_NAME}`)) {
+        console.log('Creating directory...');
+        fs.mkdirSync(`./${DIR_NAME}`);
+    }
     
     console.log('Writing blocks...');
-    var dir = './data';
-
-    if (!fs.existsSync(dir)) {
-        fs.mkdirSync(dir);
-    }
-
     fs.writeFile(
-        `${dir}/blocks_mainnet.json`,
+        `${DIR_NAME}/${FILE_NAME}`,
         JSON.stringify(blocks),
         (err) => {
             if(err) throw err;
